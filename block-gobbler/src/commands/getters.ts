54d458c68fb6444ed22ca688fd441ddfa8acd8b4
Matt Evenson <mevenson@usc.edu>
2022-05-23 12:04:38 -0700

    move fast break things

--- block-gobbler/src/commands/getters.ts
+++ block-gobbler/src/commands/getters.ts
@@ -1,5 +1,8 @@
 import {Command, Flags} from '@oclif/core'
-import { contractFlags, rpcFlags } from '../utils/flags'
+import Web3 from 'web3'
+import {contractFlags, rpcFlags} from '../utils/flags'
+import * as fs from 'node:fs'
+import {Example} from '@oclif/core/lib/interfaces'
 
 export default class Getters extends Command {
   static description = 'describe the command here'
@@ -7,10 +10,22 @@ export default class Getters extends Command {
   static flags = {
     ...rpcFlags,
     ...contractFlags,
-    getter: Flags.string({ char: 'g', required: true, multiple: true }), // multicall
+    getter: Flags.string({char: 'g', required: true, multiple: true}), // multicall
   }
+
+  static examples: Example[] = [
+    './bin/dev getters --rpc https://eth-mainnet.alchemyapi.io/v2/K-4XLGXimXhs-VxcLLUoxhpbJIFROYbz --abi ../examples/aloe-blend/abi.json --address 0x33cB657E7fd57F1f2d5f392FB78D5FA80806d1B4 -g getInventory',
+  ]
 
   public async run(): Promise<void> {
     const {flags} = await this.parse(Getters)
+
+    const web3 = new Web3(flags.rpc)
+
+    const abiJson = JSON.parse(fs.readFileSync(flags.abi, 'utf-8'))
+
+    const contract = new web3.eth.Contract(abiJson, flags.address)
+
+    console.log(contract)
   }
 }
--- block-gobbler/src/utils/flags.ts
+++ block-gobbler/src/utils/flags.ts
@@ -1,14 +1,18 @@
-import { Flags } from "@oclif/core";
+import {Flags} from '@oclif/core'
 
 export const contractFlags = {
-    abi: Flags.string({required: true}),
-    address: Flags.string({ char: 'a', required: true }),
+  abi: Flags.string({required: true}), // TODO: make optional and use Etherscan as backup
+  address: Flags.string({char: 'a', required: true}),
 }
 
 export const rpcFlags = {
-    batchSize: Flags.integer({char: 'b', required: true}),
-    concurrency: Flags.integer({ char: 'c', required: true }),
-    startBlock: Flags.integer({ char: 's', default: 0 }),
-    confirmations: Flags.integer({ char: 's', default: 12 }),
-    ethereumRpc: Flags.string({ char: 'r', required: true })
+  batchSize: Flags.integer({char: 'b', default: 100}),
+  concurrency: Flags.integer({char: 'c', default: 100}),
+  startBlock: Flags.integer({char: 's', default: 0}),
+  confirmations: Flags.integer({char: 's', default: 12}),
+  rpc: Flags.string({char: 'r', required: true}),
+}
+
+export const etherscanFlags = {
+  etherscanApiKey: Flags.string({}),
 }
